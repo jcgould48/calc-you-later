b9976285ccfe8cc1d2917c62e8675c6091a76e2e
Jesse Gould <jesse.gould@codeimmersives.com>
2019-10-07 14:50:58 -0400

    fixed interface

--- calc-you-later.js
+++ calc-you-later.js
@@ -6,7 +6,7 @@ const num1 = process.argv[2];
 const num2 = process.argv[3];
 const enteredOp = process.argv[4];
 
-const answer = calculate(num1, num2, enteredOp);
+const answer = calculate(num1,enteredOp ,num2);
 
 console.log(answer);
   
