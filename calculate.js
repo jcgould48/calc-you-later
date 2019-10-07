e8f6024a6ea02069f7d94e8db3b1f3ad07f21aa2
Jesse Gould <jesse.gould@codeimmersives.com>
2019-10-07 13:36:57 -0400

    modifying calculator

--- calculate.js
+++ calculate.js
@@ -1,6 +1,33 @@
-function calculate() {
+
+
+function calculate(num1, num2, enteredOp) {
   
+  const convertedNum1 = Number(num1);
+  const convertedNum2 = Number(num2);
+  const operation = enteredOp.toLowerCase();
+
+  if(operation === '+' || operation === 'plus'){
+    return convertedNum1 + convertedNum2;
+  }
+  else if (operation === '-'|| operation === 'minus'){
+    return convertedNum1 - convertedNum2;
+  }
+  else if (operation === 'x' || operation === 'times'){
+      return convertedNum1 * convertedNum2;
+  }
+  else if (operation === '/' || operation === 'divided by'){
+      return convertedNum1 / convertedNum2;
+  }
+  else if (operation === '%' || operation === 'modulus'  || operation === 'mod'){
+    return convertedNum1 % convertedNum2;
 }
+else {
+    return "Sorry, that's not a mathematical operation!";
+}
+
+  } 
+
+
 
 
 module.exports = calculate;
