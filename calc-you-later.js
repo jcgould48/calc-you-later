da0ad2756a5c582dbd7d3cd94363e79fc50d7b80
Jesse Gould <jesse.gould@codeimmersives.com>
2019-10-07 14:14:01 -0400

    extra tests

--- calc-you-later.js
+++ calc-you-later.js
@@ -1,1 +1,5 @@
 const calculate = require('./calculate')
+
+const num1 = process.argv[2];
+const num2 = process.argv[3];
+const enteredOp = process.argv[4];
--- calculate.js
+++ calculate.js
@@ -6,28 +6,27 @@ function calculate(num1, num2, enteredOp) {
   const convertedNum2 = Number(num2);
   const operation = enteredOp.toLowerCase();
 
-  if(operation === '+' || operation === 'plus'){
+  if(operation === '+' || operation === 'plus' || operation.substring(0,3)==='add' ){
     return convertedNum1 + convertedNum2;
   }
-  else if (operation === '-'|| operation === 'minus'){
+  else if (operation === '-'|| operation === 'minus'|| operation.substring(0,3)=== 'sub'){
     return convertedNum1 - convertedNum2;
   }
-  else if (operation === 'x' || operation === 'times'){
+  else if (operation === 'x' || operation === 'times'|| operation.substring(0,3)=== 'mul'){
       return convertedNum1 * convertedNum2;
   }
-  else if (operation === '/' || operation === 'divided by'){
+  else if (operation === '/' || operation.substring(0,3) === 'div'){
       return convertedNum1 / convertedNum2;
   }
-  else if (operation === '%' || operation === 'modulus'  || operation === 'mod'){
+  else if (operation === '%' || operation.substring(0,3) === 'mod'){
     return convertedNum1 % convertedNum2;
 }
 else {
     return "Sorry, that's not a mathematical operation!";
 }
-
   } 
 
-
+  3 multiplied by 5
 
 
 module.exports = calculate;
